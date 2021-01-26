c955e650b4c8f8846d44167a479819de0a01ef22
Ivan_WEB <ivan.exceedteam@gmail.com>
2021-01-26 16:06:18 +0300

    add grid, and load pictures

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import './App.css'
+import "./App.css";
+
+const givePicture = () => {
+  let pic = [];
+  for (let count = 1; count <= 30; count++) {
+    pic.push(<img src={`/images/space3_${count}.jpg`} alt="" />);
+  }
+  return pic;
+};
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>Pictures</h1>
-			</header>
-			<div className="container">
-				<div className="list_pictures">
-					<div className="list_pictures_item">
-						<img src="" alt="" />
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Pictures</h1>
+      </header>
+      <div className="container">
+        <div className="list_pictures">
+          {givePicture().map((el, index) => {
+            return (
+              <div className="list_pictures_item" key={index}>
+                {" "}
+                {el}{" "}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
