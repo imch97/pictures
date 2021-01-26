4ef29a747da1f32b975c4034d96bedca4600ec19
Ivan_WEB <ivan.exceedteam@gmail.com>
2021-01-26 17:36:00 +0300

    Add new styles for list picture

--- src/components/modal_window/modal_window.jsx
+++ src/components/modal_window/modal_window.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 
+const comments = [
+  {
+    user: "user1",
+    text: "Вау вау",
+    date: "yyyy-mm-dd",
+  },
+  {
+    user: "user2",
+    text: "хи-хи",
+    date: "yyyy-mm-dd",
+  },
+  {
+    user: "user3",
+    text: "Круто!",
+    date: "yyyy-mm-dd",
+  },
+  {
+    user: "user4",
+    text: "Чётко",
+    date: "yyyy-mm-dd",
+  },
+];
+
 const ModalWindow = (props) => {
-  return <div></div>;
+  const { image } = props;
+
+  return <></>;
 };
