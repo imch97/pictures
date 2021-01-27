44d38008abadc921016ca8a4c5bc3b3c13cc3841
Ivan_WEB <ivan.exceedteam@gmail.com>
2021-01-27 10:06:31 +0300

    Create structure modal window

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import "./App.css";
+import './App.css'
 
 const givePicture = () => {
-  let pic = [];
-  for (let count = 1; count <= 30; count++) {
-    pic.push(
-      <img src={`/images/space3_${count}.jpg`} key={`img_${count}`} alt="" />
-    );
-  }
-  return pic;
-};
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Pictures</h1>
-      </header>
-      <div className="container">
-        <div className="list_pictures">
-          {givePicture().map((el, index) => {
-            return (
-              <div className="list_pictures_item" key={`item_${index}`}>
-                {" "}
-                {el}{" "}
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+	let pic = []
+	for (let count = 1; count <= 30; count++) {
+		pic.push(
+			<img src={`/images/space3_${count}.jpg`} key={`img_${count}`} alt="" />
+		)
+	}
+	return pic
 }
 
-export default App;
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Pictures</h1>
+			</header>
+			<div className="container">
+				<div className="list_pictures">
+					{givePicture().map((el, index) => {
+						return (
+							<div className="list_pictures_item" key={`item_${index}`}>
+								{' '}
+								{el}{' '}
+							</div>
+						)
+					})}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
--- src/components/modal_window/modal_window.jsx
+++ src/components/modal_window/modal_window.jsx
@@ -1,34 +1,61 @@
-import React from "react";
+import React, { useState } from 'react'
 
 const comments = [
-  {
-    user: "user1",
-    text: "Вау вау",
-    date: "yyyy-mm-dd",
-  },
-  {
-    user: "user2",
-    text: "хи-хи",
-    date: "yyyy-mm-dd",
-  },
-  {
-    user: "user3",
-    text: "Круто!",
-    date: "yyyy-mm-dd",
-  },
-  {
-    user: "user4",
-    text: "Чётко",
-    date: "yyyy-mm-dd",
-  },
-];
+	{
+		user: 'user1',
+		text: 'Вау вау',
+		date: 'yyyy-mm-dd',
+	},
+	{
+		user: 'user2',
+		text: 'хи-хи',
+		date: 'yyyy-mm-dd',
+	},
+	{
+		user: 'user3',
+		text: 'Круто!',
+		date: 'yyyy-mm-dd',
+	},
+	{
+		user: 'user4',
+		text: 'Чётко',
+		date: 'yyyy-mm-dd',
+	},
+]
 
 const ModalWindow = (props) => {
-  const { image } = props;
+	const { image } = props
 
-  return (
-    <>
-      <div className="modal_window"></div>
-    </>
-  );
-};
+	const [visible, setVisible] = useState(false)
+	const [tittle, setTittle] = useState('')
+	const [text, setText] = useState('')
+
+	function closeModal() {}
+	function openModal(image, comments) {
+		setVisible(true)
+	}
+
+	return (
+		<>
+			<div className="backdrop">
+				<div className="modal_window">
+					<div className="modal_window_picture">
+						<img src="" alt="" />
+					</div>
+					<div className="modal_window_comments">
+						<div className="window_comments">
+							<div className="comment">
+								<div className="comment_info">
+									<div className="comment_info_user"></div>
+									<div className="comment_info_date"></div>
+								</div>
+								<div className="comment_text"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+module.exports = ModalWindow
